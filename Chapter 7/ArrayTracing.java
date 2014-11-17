94dd6f011541b2461eadfaf67c2f58441f0873fb
famalone <finmalone@gmail.com>
2014-11-17 11:26:32 -0600

    algorithms

---
+++ Chapter 7/ArrayTracing.java
@@ -0,0 +1,13 @@
+
+public class ArrayTracing
+{
+    public static void main(String[] args)
+    {
+        int[] a = { 1, 2, 3, 4, 5, 4, 3, 2, 1, 0 };
+
+        for(int i = 0; i < 10; i = i++)
+        {
+            int total = 0;
+        }
+    }
+}
